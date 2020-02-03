e47f8d3a71770d03e14e8418d67a85ed2410e257
thebidixhd <bidiboy012@gmail.com>
2020-02-03 19:26:44 +0100

    2.5 done

--- config.ru
+++ config.ru
@@ -3,4 +3,5 @@ Bundler.require
 
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'controller'
+
 run ApplicationController
--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,22 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base
   get '/' do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all}
   end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    puts "Salut, je suis dans le serveur"
+    puts "Ceci est le contenu du hash params : #{params}"
+    puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+    puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+    puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+    Gossip.new(params["gossip_author"],params["gossip_content"]).save
+    redirect '/'
+  end
+
 end
